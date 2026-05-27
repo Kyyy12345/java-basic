0468c299241edb81b1767018c3220639e6bd67fa
Kyyy12345 <rladbsduf135@naver.com>
2026-05-27 23:25:26 +0900

    접근제어자 실십중

---
+++ src/access/a/AccessInnerMain.java
@@ -0,0 +1,19 @@
+package access.a;
+
+public class AccessInnerMain {
+    static void main() {
+        AccessData data = new AccessData();
+
+        // public 호출 가능
+        data.publicField = 1;
+        data.publicMethod();
+
+        // 같은 패키지 default 호출 가능
+        data.defaultField = 10;
+        data.defaultMethod();
+
+        // private 호출 불가
+//        data.privateField = 3;
+//        data.privateMethod();
+    }
+}
---
+++ src/pack/a/PackageMain1.java
@@ -0,0 +1,10 @@
+package pack.a;
+
+import pack.Data;
+
+public class PackageMain1 {
+    static void main() {
+        Data data = new Data();
+        User user = new User();
+    }
+}
